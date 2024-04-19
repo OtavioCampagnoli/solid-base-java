4617094ee7cb784a6143f6ce4396e4cab55a339f
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-04-19 14:43:30 -0300

    Conditionals Java Concept: Even or Odd.

---
+++ src/conditionals/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package conditionals;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 24;
+
+        if (number % 2 == 0) {
+            System.out.println("The number: " + number + " is Even.");
+        } else {
+            System.out.println("The number: " + number + " is Odd.");
+        }
+    }
+}
